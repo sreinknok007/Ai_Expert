8a4b87624d91725e5a41dd54bbc9fa7401636fb6
sreinknok007 <sreinknokyt@gmail.com>
2025-11-02 10:54:03 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -1,12 +1,28 @@
 import requests
+from requests import api
 
-def grf():
-    url = "https://uselessfacts.jsph.pl/random.json?language=en"
-    responce = requests.get(url)
-    if responce.status_code == 200:
-        d = responce.json()
-        print("Not Funny Facts")
-        print(d["text"])
-    else:
-        print("Not Working Slow internet")
-grf()
+API_KEY = "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"
+
+api_url = "https://api-inference.huggingface.co/models/nlptown/bert-base-multilingual-uncased-sentiment"
+
+headers = {
+    "Authorization": f"Bearer {API_KEY}"
+}
+
+text = "I think Dollar will lose shinr"
+
+response = requests.post(api_url,headers=headers,json={'inputs':text})
+
+if response.status_code == 200:
+    
+    classification = response.json()
+
+# print(classification)
+
+    print("Predicted label:", classification[0][0]['label'])
+
+    print("Confidence:", classification[0][0]['score'])
+
+else:
+
+ print(f"Error: {response.status_code}")
